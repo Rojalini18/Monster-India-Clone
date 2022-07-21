6a49425cdc7c3cf611a69c762c23efbe29c8198d
Mamata  Ingole <mamataingole125@gmail.com>
2022-07-21 23:15:14 +0530

    Career Tips page added

--- login-signup/src/App.js
+++ login-signup/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Login from "./Pages/Login"
 import Signup from "./Pages/Signup"
+import CareerTips from './Pages/CareerTips';
 
 function App() {
   return (
     <div className="App">
-    <Login/>
-    <Signup/>
+    {/* <Login/> */}
+    {/* <Signup/> */}
+    <CareerTips/>
     </div>
   );
 }
